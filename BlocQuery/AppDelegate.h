257c33cc40e5064881090fffd7718331cd574a92
Steve Palley <spall03@gmail.com>
2015-01-12 13:54:56 -0800

    Initial Commit

---
+++ BlocQuery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocQuery
+//
+//  Created by Stephen Palley on 1/12/15.
+//  Copyright (c) 2015 Steve Palley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocQuery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocQuery
+//
+//  Created by Stephen Palley on 1/12/15.
+//  Copyright (c) 2015 Steve Palley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
