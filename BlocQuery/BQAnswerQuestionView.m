56c9b80db8a25328f3061a141a034af710960d59
Matt Hanlon <mhanlon@gmail.com>
2015-01-30 12:55:27 -0800

    Tweaking our answering layout.

--- BlocQuery/BQAnswerQuestionView.m
+++ BlocQuery/BQAnswerQuestionView.m
@@ -12,6 +12,8 @@
 
 @interface BQAnswerQuestionView () <UITextViewDelegate>
 
+@property (nonatomic, assign) BOOL isAnswering;
+
 @end
 
 @implementation BQAnswerQuestionView
@@ -32,15 +34,17 @@
         
         //setup a button to submit new answers
         self.submitButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        self.submitButton.backgroundColor = [UIColor whiteColor];
         [self.submitButton setTitle:@"Submit" forState:UIControlStateNormal];
         [self.submitButton addTarget:self action:@selector(submitButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
        
         
         //setup a button to submit new answers
         self.cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        self.cancelButton.backgroundColor = [UIColor whiteColor];
         [self.cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
         [self.cancelButton addTarget:self action:@selector(cancelButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
-    
+
         
         //display the question text first
         NSString *questionString = [NSString stringWithFormat:@"%@", self.question.questionText];
@@ -52,63 +56,71 @@
         [self addSubview:self.questionLabel];
         [self addSubview:self.textView];
         
-        [self.textView addSubview:self.submitButton];
-        [self.textView addSubview:self.cancelButton];
-        
-        self.questionLabel.translatesAutoresizingMaskIntoConstraints = NO;
-        self.textView.translatesAutoresizingMaskIntoConstraints = NO;
-        
+        [self addSubview:self.submitButton];
+        [self addSubview:self.cancelButton];
+
+        self.isAnswering = NO;
         
     }
     return self;
+}
+
+- (void)didMoveToSuperview
+{
+    [super didMoveToSuperview];
+    [self.questionLabel sizeToFit];
 }
 
 //center our question label in the middle of our label view
 - (void)layoutSubviews
 {
     [super layoutSubviews];
-    [self.questionLabel sizeToFit];
+    if ( self.isAnswering )
+    {
+        float bottommostY = CGRectGetMaxY( self.questionLabel.frame ); // TODO: For now we'll pin our text view to the bottom of our question view... though before
+        
+        self.textView.hidden = NO;
+        self.textView.frame = CGRectMake(self.bounds.origin.x, bottommostY, self.bounds.size.width, 400.0);
+        // TODO: Is the intention that the text view drops so far below the question label?
+        self.textView.backgroundColor = [UIColor lightGrayColor];
+        self.textView.editable = YES;
+        self.textView.userInteractionEnabled = YES;
     
-    NSLayoutConstraint *xCenterConstraint = [NSLayoutConstraint constraintWithItem:self.questionLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
-    [self addConstraint:xCenterConstraint];
-    
-    NSLayoutConstraint *yCenterConstraint = [NSLayoutConstraint constraintWithItem:self.questionLabel attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0];
-    [self addConstraint:yCenterConstraint];
-
-    
+        self.submitButton.hidden = NO; // TODO: These guys are no longer subviews of our text view.
+        self.cancelButton.hidden = NO;
+        [self.submitButton sizeToFit];
+        [self.cancelButton sizeToFit];
+        
+        self.cancelButton.frame = CGRectMake(
+                                             ( CGRectGetMaxX( self.textView.frame ) - ( self.cancelButton.frame.size.width + self.submitButton.frame.size.width + 50 ) ),
+                                             ( bottommostY + ( self.cancelButton.frame.size.height + 25 ) ),
+                                             self.cancelButton.frame.size.width,
+                                             self.cancelButton.frame.size.height );
+        self.submitButton.frame = CGRectMake(
+                                             ( CGRectGetMaxX( self.textView.frame ) - ( self.submitButton.frame.size.width + 30 )),
+                                             ( bottommostY + ( self.submitButton.frame.size.height + 25 ) ),
+                                             self.submitButton.frame.size.width,
+                                             self.submitButton.frame.size.height );
+        self.submitButton.userInteractionEnabled = YES;
+        self.cancelButton.userInteractionEnabled = YES;
+    }
+    else
+    {
+        self.textView.hidden = YES;
+        self.submitButton.hidden = YES; // TODO: These guys are no longer subviews of our text view.
+        self.cancelButton.hidden = YES;
+    }
 }
 
 - (void)startTextEditing
 {
+    self.isAnswering = YES;
     NSLog(@"start text editing!");
-
-    
-    float bottomLeftCorner = CGRectGetMaxY(self.questionLabel.frame);
-    
-    //TODO: fix all of this for autolayout
-    
-    
-    self.textView.hidden = NO;
-    self.textView.frame = CGRectMake(self.bounds.origin.x, bottomLeftCorner, self.bounds.size.width, 400.0);
     self.textView.text = @"Answer here.";
-    self.textView.backgroundColor = [UIColor lightGrayColor];
-    self.textView.editable = YES;
-    self.textView.userInteractionEnabled = YES;
-    
-    
-    //FIXME: can't figure out why these buttons will only fire in this position!
-    self.cancelButton.frame = CGRectMake(100.0, 0.0, 100.0, 100.0);
-    self.submitButton.frame = CGRectMake(250.0, 0.0, 100.0, 100.0);
-    
-    [self.submitButton sizeToFit];
-    [self.cancelButton sizeToFit];
-    
-    self.submitButton.backgroundColor = [UIColor whiteColor];
-    self.cancelButton.backgroundColor = [UIColor whiteColor];
     
     self.submitButton.userInteractionEnabled = YES;
     self.cancelButton.userInteractionEnabled = YES;
-    
+    [self layoutSubviews];
 }
 
 - (void)submitButtonPressed:(id)sender
@@ -120,13 +132,13 @@
     [[BQUser currentUser] addNewAnswer:self.textView.text toQuestion:self.question];
     
     //reset textfield and put it away
-    self.textView.text = @"Answer here.";
-    self.textView.hidden = YES;
+    self.textView.text = @"Answer here."; // TODO: Do you want this to override what the user has typed and maybe canceled? Do you want to leave their typed text if they cancel and then decide to try and re-answer?
     
     //need to then refresh the parent view controller
     [self.delegate answerQuestionViewDidAddAnswer:self];
     
-    
+    self.isAnswering = NO;
+    [self layoutSubviews];
 }
 
 - (void)cancelButtonPressed:(id)sender
@@ -134,9 +146,8 @@
 
     NSLog(@"Cancel comment!");
 
-    self.textView.hidden = YES;
-    
-    
+    self.isAnswering = NO;
+    [self layoutSubviews];
 }
 
 
