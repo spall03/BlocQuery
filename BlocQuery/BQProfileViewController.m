102a0149f69bab1f0196c0be137db868d0d13a44
Steve Palley <spall03@gmail.com>
2015-02-05 16:00:18 -0800

    basic user profile complete. still need profile editing and decent-looking UI.

--- BlocQuery/BQProfileViewController.m
+++ BlocQuery/BQProfileViewController.m
@@ -15,10 +15,13 @@
 @interface BQProfileViewController ()
 
 @property (nonatomic, strong) BQUser *user;
-@property (nonatomic, strong) PFImageView *userImage;
-@property (nonatomic, strong) NSString *userDescription;
+//@property (nonatomic, strong) PFFile *userImage;
+//@property (nonatomic, strong) NSString *userDescription;
+@property (nonatomic, strong) NSString *placeholderDescription;
 @property BOOL isCurrentUser;
 
+@property (nonatomic, strong) PFImageView *userImageView;
+@property (nonatomic, strong) UIImage *placeholderImage;
 @property (nonatomic, strong) UITextView *userDescriptionTextView;
 @property (nonatomic, strong) UIButton *editUserImageButton;
 @property (nonatomic, strong) UIButton *editUserDescriptionButton;
@@ -35,6 +38,13 @@
     {
         self.user = user;
     }
+    
+    //load up the config file to get default values
+    PFConfig *config = [PFConfig getConfig];
+    PFFile *tempImageFile = config[@"BQDefaultUserImage"];
+    NSData *tempImageData = [tempImageFile getData];
+    self.placeholderImage = [UIImage imageWithData:tempImageData]; //placeholder image goes in no matter what b/c that's how PFImageView works
+    self.placeholderDescription = config[@"BQDefaultUserDescription"];
     
     [self determineUser];
     [self loadData];
@@ -58,7 +68,9 @@
 //attempt to load user data from cloud. if no user data, load defaults from config
 - (void) loadData
 {
-    //first, try to load up the user's info from the cloud
+    
+    
+    //try to load the user's info from the cloud
     PFQuery *userQuery = [PFUser query];
     [userQuery whereKey:@"username" equalTo:self.user.username];
     [userQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
@@ -73,31 +85,32 @@
              //if it works and user already has a description, load that description into the textview
              if (![temp[@"additional"] isEqual: @""])
              {
-                 NSLog(@"User data exists");
-                 self.userDescription = temp[@"additional"];
-                 self.userDescriptionTextView.text = self.userDescription;
+                 NSLog(@"User description exists");
+                 self.userDescriptionTextView.text = temp[@"additional"];
              }
              
-             //if no user data, grab description from config
+
              else
              {
-                 NSLog(@"No user data!");
-                 
-                 [PFConfig getConfigInBackgroundWithBlock:^(PFConfig *config, NSError *error) {
-                     if (config)
-                     {
-                         NSLog(@"Config fetched!");
-                         
-                         self.userDescription = config[@"BQDefaultUserDescription"];
-                         self.userDescriptionTextView.text = self.userDescription;
-                     }
-                     else
-                     {
-                         NSLog(@"Defaults didn't load, and here's why: %@", error);
-                     }
-                 }];
-                 
+                 NSLog(@"No user description!");
+                 self.userDescriptionTextView.text = self.placeholderDescription;
              }
+             
+             //do the same thing for the user image
+             if (temp[@"userImage"])
+             {
+                 NSLog(@"User image exists!");
+                 self.userImageView.file = temp[@"userImage"];
+                [self.userImageView loadInBackground]; //??? will this redraw the view again once it loads?
+             }
+             
+             else
+             {
+                 NSLog(@"No user image!");
+             }
+             
+             //this will display all the user data we got from the cloud
+             [self.view layoutSubviews];
              
          }
          
@@ -108,11 +121,10 @@
          
      }];
     
-    //refresh user image from cloud
-    //   [self.userImage loadInBackground];
     
 }
 
+//set up with placeholders because the real stuff is coming asynchronously
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -121,20 +133,33 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     self.userDescriptionTextView = [[UITextView alloc]init];
-    self.userImage = [[PFImageView alloc]init];
-    self.editUserImageButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    self.editUserDescriptionButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.userDescriptionTextView.text = @"Loading...";
     
-    PFConfig *config = [PFConfig getConfig];
-    PFFile *temp = config[@"BQDefaultUserImage"];
-    NSData *data = [temp getData];
-    UIImage *image = [UIImage imageWithData:data];
-    self.userImage.image = image;
+    //set the user image's default and actual values, then pull actual from cloud
+    self.userImageView = [[PFImageView alloc]init];
+    self.userImageView.image = self.placeholderImage;
+
 
     [self.view addSubview:self.userDescriptionTextView];
-    [self.view addSubview:self.userImage];
+    [self.view addSubview:self.userImageView];
+    
+    //enable editing buttons if you're looking at your own profile
+    if (self.isCurrentUser)
+    {
+        self.editUserImageButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [self.editUserImageButton setTitle:@"Change Photo" forState:UIControlStateNormal];
+        [self.editUserImageButton addTarget:self action:@selector(editImageButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        
+        self.editUserDescriptionButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [self.editUserDescriptionButton setTitle:@"Edit Description" forState:UIControlStateNormal];
+        [self.editUserDescriptionButton addTarget:self action:@selector(editDescriptionButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self.view addSubview:self.editUserImageButton];
+        [self.view addSubview:self.editUserDescriptionButton];
+    }
 
 }
+
 
 - (void)viewDidLayoutSubviews
 {
@@ -144,8 +169,22 @@
     self.userDescriptionTextView.frame = CGRectMake(0, 0, self.view.bounds.size.width, 200);
     self.userDescriptionTextView.backgroundColor = [UIColor lightGrayColor];
     self.userDescriptionTextView.hidden = NO;
+    self.userDescriptionTextView.userInteractionEnabled = NO; //need to hit edit button first
     
-    self.userImage.frame = CGRectMake(0, 200, self.view.bounds.size.width, 200);
+    self.userImageView.frame = CGRectMake(0, 200, self.view.bounds.size.width, 200);
+    
+    if (self.isCurrentUser)
+    {
+    
+        self.editUserImageButton.frame = CGRectMake(100, 400, 100, 100);
+        self.editUserDescriptionButton.frame = CGRectMake(100, 500, 100, 100);
+        
+        [self.editUserImageButton sizeToFit];
+        [self.editUserDescriptionButton sizeToFit];
+    
+    }
+    
+    
     
     
 }
@@ -154,6 +193,26 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma button handling
+
+-(void) editImageButtonPressed:(id)sender
+{
+    
+    NSLog(@"Edit image pressed!");
+    
+    
+}
+
+-(void) editDescriptionButtonPressed:(id)sender
+{
+    
+    NSLog(@"Edit description pressed!");
+    
+    
+}
+
 
 /*
 #pragma mark - Navigation
